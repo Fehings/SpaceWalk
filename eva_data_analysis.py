509d3d31728980e6b994e25ad0eec7ef1ecb3c4c
Fehings <frances.hutchings@newcastle.ac.uk>
2025-04-03 10:29:43 +0100

    refactored to functions

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -6,24 +6,31 @@ This uses data from https://data.nasa.gov/resource/eva.json (with modifications)
 The script will plot and save a figure of cumulative time in space.
 '''
 
-print(f'Reading input file {data_in}')
+def read_json_to_dataframe(input_file):
+    # read in the data as a pandas dataframe
+    eva_data= pd.read_json(input_file, convert_dates=['date'])
+    # convert eva colunm to float
+    print('Cleaning data')
+    eva_data['eva'] = eva_data['eva'].astype(float)
+    # get rid of missing values
+    eva_data.dropna(axis=0, inplace=True)
+    # sort by date
+    eva_data.sort_values('date',inplace=True)
+    return eva_data
+
+def write_dataframe_to_csv(df, outfile):
+    df.to_csv(outfile, index=False)
+
+
+
 # Opening input and output files and setting graph file name
-data_in = open('./eva-data.json', 'r')
-data_out = open('./eva-data.csv','w')
+input_file = open('./eva-data.json', 'r', encoding="utf-8")
+eva_data=read_json_to_dataframe(input_file)
+
+write_dataframe_to_csv(eva_data,'./eva-data.csv')
+
 graph_file = 'myplot.png'
 
-print('Cleaning data')
-# read in the data as a pandas dataframe
-eva_data= pd.read_json(data_in,convert_dates['date'])
-# convert eva colunm to float
-eva_data['eva'] = eva_data['eva'].astype(float)
-# get rid of missing values
-eva_data.dropna(axis=0, inplace=True)
-# sort by date
-eva_data.sort_values('date',inplace=True)
-
-# write sorted and cleaned data to csv
-eva_data.to_csv(data_out, index=False)
 
 print('Plotting cumulative space walk data')
 # plot the cumulative time spent in space and save the figure
